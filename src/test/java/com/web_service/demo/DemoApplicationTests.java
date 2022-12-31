9981d76dd06845f2f83f3e02e5b72b5aace277af
swh <tjdngur22@gmail.com>
2022-12-31 17:17:21 +0900

    first commit + gitignore 등록

---
+++ src/test/java/com/web_service/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.web_service.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
