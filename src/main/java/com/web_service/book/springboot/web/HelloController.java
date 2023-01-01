5b872e739de47762d8041ee7dc253526e4b2ebc4
swh <tjdngur22@gmail.com>
2023-01-01 09:39:59 +0900

    HelloResponseDto 컨트롤추가 + test코드 작성

--- src/main/java/com/web_service/book/springboot/web/HelloController.java
+++ src/main/java/com/web_service/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.web_service.book.springboot.web;
 
+import com.web_service.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +14,16 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+
+    @GetMapping("hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        //@RequistParam
+        //외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+        //여기서는 외부에서 name(@RequestParam("name"))이란 이름으로 넘긴 파라미터를 메소드
+        // 파라미터 name(String name)에 저장한다.
+        return new HelloResponseDto(name, amount);
+    }
+
 }
--- src/test/java/com/web_service/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/web_service/book/springboot/web/HelloControllerTest.java
@@ -1,8 +1,10 @@
 package com.web_service.book.springboot.web;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 import com.web_service.book.springboot.web.HelloController;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +13,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import java.util.regex.Matcher;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -31,4 +34,28 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+    //param
+    //API 테스트를 할때 사용될 요청 파라미터를 설정한다.
+    //단, 값은 Strin만 허용된다.
+    //그래서 숫자/날짜 등의 데이터도 등록 할때는 문자열로 변경해야만 가능
+
+    //jsonPath
+    //JSON응답값을 필드별로 검증 할 수있느느 메소드입니다.
+    //$를 기준으로 필드명을 암시한다.
+    //여기서는 name과 amount를 검증하니 $name, $amount로 검증한다.
+
+
 }
