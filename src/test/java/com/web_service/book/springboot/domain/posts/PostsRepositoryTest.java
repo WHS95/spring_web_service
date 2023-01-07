9ab659ce745a5f1aed62982d2750fafb6a05d204
swh <tjdngur22@gmail.com>
2023-01-07 20:46:21 +0900

    jpa test code 작성

--- src/test/java/com/web_service/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/web_service/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -19,6 +19,8 @@ public class PostsRepositoryTest {
     PostsRepository postsRepository;
 
     @After
+    //Junit에서 단위테스트가 끝날때마다 수행되는 메소드를 지정
+    //보통은 배포전 전체 테스트를 수행할 때 테슽간 데이터 침범을 막기 위해 사용
     public void cleanup() {
         postsRepository.deleteAll();
     }
@@ -29,12 +31,15 @@ public class PostsRepositoryTest {
         String title = "테스트 게시글";
         String content = "테스트 본문";
 
+        //테이블 posts에 insert/update
+        //id값이 있다면 update가, 없다면 insert쿼리가 실행된다.
         postsRepository.save(Posts.builder().title(title)
                 .content(content)
                 .author("tjrdlf08@gmail.com")
                 .build());
 
         //when
+        //테이블 posts에 있는 모든 데이터를 조회해오는 메소드이다.
         List<Posts> postsList = postsRepository.findAll();
 
         //then
