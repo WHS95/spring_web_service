7ef7f133a3705cc5ff4e80f0ad4985f6156fd36a
swh <tjdngur22@gmail.com>
2023-01-07 21:17:51 +0900

    등록api 구성

---
+++ src/main/java/com/web_service/book/springboot/service/PostsService.java
@@ -0,0 +1,22 @@
+package com.web_service.book.springboot.service;
+
+import com.web_service.book.springboot.domain.posts.Posts;
+import com.web_service.book.springboot.domain.posts.PostsRepository;
+import com.web_service.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
---
+++ src/main/java/com/web_service/book/springboot/web/PostsApiController.java
@@ -0,0 +1,27 @@
+package com.web_service.book.springboot.web;
+
+import com.web_service.book.springboot.service.PostsService;
+import com.web_service.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
