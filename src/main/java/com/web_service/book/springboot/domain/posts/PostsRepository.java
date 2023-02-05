883430368af4e0102a28ab0e591d05e2a488899c
swh <tjdngur22@gmail.com>
2023-02-05 13:47:15 +0900

    "게시글 전체 조회 화면구성및 기능 구현 + 수정기능 반영 확인"

--- src/main/java/com/web_service/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/web_service/book/springboot/domain/posts/PostsRepository.java
@@ -6,12 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 @Repository
 public interface PostsRepository extends JpaRepository<Posts,Long> {
-    void deleteByAuthorAllIgnoreCase(String author);
-    @Transactional
-    @Modifying
-    @Query("delete from Posts p")
-    int deleteFirstByAllIgnoreCase();
+//    void deleteByAuthorAllIgnoreCase(String author);
+//    @Transactional
+//    @Modifying
+//    @Query("delete from Posts p")
+//    int deleteFirstByAllIgnoreCase();
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/web_service/book/springboot/service/PostsService.java
+++ src/main/java/com/web_service/book/springboot/service/PostsService.java
@@ -6,9 +6,13 @@ import com.web_service.book.springboot.domain.posts.PostsRepository;
 import com.web_service.book.springboot.web.dto.PostsResponseDto;
 import com.web_service.book.springboot.web.dto.PostsSaveRequestDto;
 import com.web_service.book.springboot.web.dto.PostsUpdateRequestDto;
+import com.web_service.book.springboot.web.dto.PostsListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -31,11 +35,26 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)//트랜젝션 범위는 유지하되 조회기능만 남겨두어 조회속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)//(posts-> new PostsListResposeDto(posts))
+                .collect(Collectors.toList());
     }
 
 }
